eedd84c4248fe3be8c3c29e15c20368ada2d9bb6
hazzama98 <ninjahazzama@gmail.com>
2024-10-13 14:48:10 +0800

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from bot import bot
-
-if __name__ == '__main__':
-    try:
-        bot.process()
-    except KeyboardInterrupt:
-        print("Bot stopped by user...")
-        sys.exit(2)
